d1a50610d2348bd42fc873ff28d7e49d6a9e1e44
EC2 Default User <ec2-user@ip-172-31-33-28.eu-west-1.compute.internal>
2024-09-23 13:00:10 +0000

    added a workflow

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -7,12 +7,15 @@ def handler(event, context):
     client = boto3.client('comprehend')
     body = event["body"]
     sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
+    Sentiment["Sentiment"] = "POSITIVE"
+
     return {
             "statusCode": 200,
             "headers": {
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "sentiment ": json.dumps(sentiment)
+                "sentiment ": json.dumps(sentiment),
+                "Sentiment" : json.dumps(Sentiment)
             })
     }
